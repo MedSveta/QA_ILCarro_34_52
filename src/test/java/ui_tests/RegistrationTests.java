ce1b97b5cd5bf9976c696cd0158683c8843b51d7
MedSveta <sveta1978medved@gmail.com>
2026-08-23 20:57:32 +0300

    Registration Positive Test

--- src/test/java/ui_tests/RegistrationTests.java
+++ src/test/java/ui_tests/RegistrationTests.java
@@ -2,6 +2,8 @@ package ui_tests;
 
 import dto.User;
 import manager.AppManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -14,8 +16,10 @@ import static utils.UserFactory.*;
 public class RegistrationTests extends AppManager {
     RegistrationPage registrationPage;
 
+
     @BeforeMethod
     public void goToRegistrationPage() {
+       logger.info("Start registration test");
         new HomePage(getDriver()).clickBtnSignUp();
         registrationPage = new RegistrationPage(getDriver());
     }
@@ -35,6 +39,16 @@ public class RegistrationTests extends AppManager {
         User user = positiveUser();
         registrationPage.typeRegistrationForm(user);
         registrationPage.clickCheckboxTermsOfUse();
+        registrationPage.clickBtnYalla();
+        Assert.assertTrue(new PopUpPage(getDriver())
+                .isTextInPopUpMessagePresent("You are logged in success"));
+    }
+
+    @Test
+    public void registrationPositiveWithActionsTest() {
+        User user = positiveUser();
+        registrationPage.typeRegistrationForm(user);
+        registrationPage.clickCheckBoxWithActions();
         registrationPage.clickBtnYalla();
         Assert.assertTrue(new PopUpPage(getDriver())
                 .isTextInPopUpMessagePresent("You are logged in success"));
